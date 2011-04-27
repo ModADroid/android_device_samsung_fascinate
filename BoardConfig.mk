d576d40f272d29cc98e0f881a9f53dc8ed882697
jt1134 <jt1134@gmail.com>
2011-04-26 23:39:09 -0500

    fixes for UMS
    Change-Id: I64cc529d419520828b6bc00bcaa3edd45d54f3ed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,8 @@ BOARD_USES_COPYBIT := true
 DEFAULT_FB_NUM := 0
 BOARD_OVERLAY_FORMAT_YCbCr_420_SP := true
 BOARD_UMS_LUNFILE := "/sys/devices/platform/s3c-usbgadget/gadget/lun0/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun"
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 #BOARD_CAMERA_LIBRARIES := libcamera
 USE_CAMERA_STUB := true
